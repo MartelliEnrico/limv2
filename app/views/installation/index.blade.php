c7c23209e5fae9b921fa560bb17e462a87950250
MartelliEnrico <mrcnrt95e30@gmail.com>
2014-06-01 18:15:52 +0200

    Added installation process

--- app/views/installation/index.blade.php
+++ app/views/installation/index.blade.php
@@ -3,7 +3,7 @@
 @section('body')
 
 <div id="installation">
-    <p>Hai configurato adeguatamente il file <code>lim.php</code>? Allora procedi all'installazione.</p>
+    <p>Hai configurato adeguatamente il file <code>app/config/lim.php</code>? Allora procedi all'installazione.</p>
     <a href="{{ url('start') }}" class="button">Installa LimManager</a>
 </div>
 

31a50befb87a5713253db33af0b21afad8a74443
MartelliEnrico <mrcnrt95e30@gmail.com>
2014-06-13 09:41:18 +0200

    Update head.blade.php

--- app/views/layouts/partials/head.blade.php
+++ app/views/layouts/partials/head.blade.php
@@ -1,5 +1,5 @@
 <meta charset="UTF-8">
 <title>@yield('title', 'Lim Manager')</title>
-
+<meta name="viewport" content="width=device-width, user-scalable=no">
 <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
 {{ HTML::style('css/app.css') }}
